464268e47dd95fbeb5aa223a5e7601faca19551c
daRos <dario.rossim@gmail.com>
2022-04-20 14:37:28 +0200

    mayErr

--- clust3AB/plot.py
+++ clust3AB/plot.py
@@ -7,28 +7,26 @@ import sys
 N = int(sys.argv[1])
 dirname = '../Data/Data_'+sys.argv[1]+'/'
 minE = []
-E = []
-for file in os.listdir(dirname):
-    with open(dirname+file, 'r') as f:
+E = {'3x3':[],
+     'q0' :[],
+     'cb1':[]
+     }
+for filename in os.listdir(dirname):
+    with open(dirname+filename, 'r') as f:
         lines = f.readlines()
     N = (len(lines)-1)//4 + 1
-    if N == 3:
-        N = 2
     tempE = []
-    tE = []
-    if N !=  2:
-        print(file," has only ",N," ansatze.")
     for i in range(N):
-        tE.append([])
+        tE = []
         data = lines[i*4+1].split(',')
         tempE.append(float(data[3]))     #ans,J2,J3,E,S
         for j in range(5):
-            tE[i].append(data[j])
-    E.append(tE)
+            tE.append(data[j])
+        E[data[0]].append(tE)
     minInd = np.argmin(np.array(tempE))
     minE.append(lines[minInd*4+1].split(',')[:5])
 
-pts = len(E)
+pts = len(os.listdir(dirname))
 Color = {'3x3': 'b',
          'q0':   'r',
          '0-pi': 'y',
@@ -41,17 +39,17 @@ for p in range(pts):
         conv = 'o'
     plt.scatter(float(minE[p][1]),float(minE[p][2]),color=Color[minE[p][0]],marker = conv)
 plt.show()
-exit()
+
 plt.figure(figsize=(16,16))
-for i in range(5):
-    plt.subplot(2,3,i+1)
-    plt.title(E[0][i][0])
-    for p in range(pts):
+for ind,i in enumerate(['3x3', 'q0', 'cb1']):
+    plt.subplot(1,3,ind+1)
+    plt.title(i)
+    for p in range(len(E[i])):
         conv='^'
         try:
-            if float(E[p][i][4]) < 1e-8:
+            if float(E[i][p][4]) < 1e-8:
                 conv = 'o'
-            plt.scatter(float(E[p][i][1]),float(E[p][i][2]),color=Color[E[p][i][0]],marker = conv)
+            plt.scatter(float(E[i][p][1]),float(E[i][p][2]),color=Color[E[i][p][0]],marker = conv)
         except:
             continue
 
