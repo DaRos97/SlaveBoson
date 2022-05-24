ec9a7a92c57d1380629d8bdc0b1f09b33824c8d4
DaRos97 <dario.rossim@gmail.com>
2022-05-24 21:19:27 +0200

    ready for cluster

--- phase_diagram/plot.py
+++ phase_diagram/plot.py
@@ -10,8 +10,8 @@ Color = {'3x3': ['b','orange'],
          '0-pi': 'y',
          'cb1':  ['m','g'],
          'cb2': 'k'}
-dirname = '../Data/noDM/Data_13-13_2/'; title = 'Without DM interactions'
-#dirname = '../Data/yesDM/Data_13-13_new/'; title = 'With DM interactions'
+#dirname = '../Data/noDM/Data_13-13_17/'; title = 'Without DM interactions'
+dirname = '../Data/yesDM/Data_13-13_aaaa/'; title = 'With DM interactions'
 minE = []
 ct = {'3x3':10, 'q0':10,'cb1':10}
 ct2 = 1e-7
